e4d4e164cd69bf829f3bd0cc47301985e48c0e6e
박현재 <md101106@gmail.com>
2022-08-27 01:36:00 +0900

    Add Command Pattern Invoker

--- DesignPatternPP/CommandPattern.cpp
+++ DesignPatternPP/CommandPattern.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 /*
 * 
@@ -52,12 +53,37 @@ public:
 	std::shared_ptr<PrintCommand> _printCommand;
 };
 
+class CommandInvoker
+{
+public:
+	inline void AddCommand(std::shared_ptr<Command>& command)
+	{
+		_commands.emplace_back(command);
+	}
+	void Run() const
+	{
+		for (auto& command : _commands)
+		{
+			command->Execute();
+		}
+	}
+private:
+	std::vector<std::shared_ptr<Command>> _commands;
+};
+
 int main(void)
 {
 	std::shared_ptr<Command> command_01 = std::make_shared<PrintCommand>("Hello");
 	std::shared_ptr<Command> command_02 = std::make_shared<MovementCommand>(std::dynamic_pointer_cast<PrintCommand>(command_01));
-	command_01->Execute();
-	command_02->Execute();
+
+	//command_01->Execute(); 
+	//command_02->Execute();
+
+	CommandInvoker myCommandInvoker;
+	myCommandInvoker.AddCommand(command_01);
+	myCommandInvoker.AddCommand(command_02);
+
+	myCommandInvoker.Run();
 
 	return 0;
 }
